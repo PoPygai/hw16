49df9e7c288da92ec47faa53a34100579cf63f86
David Rash <DavidR@test.com>
2025-06-02 17:52:16 +0300

    change model book

---
+++ 1.js
@@ -0,0 +1,16 @@
+
+
+const book = {
+    id:1,
+    name:"Test",
+    pickList:[
+        {reader : "Albert", give_date:"01/11/2008",return_date:""},
+        {reader : "Albert1", give_date:"02/11/2008",return_date:""},
+        {reader : "Albert2", give_date:"03/11/2008",return_date:""},
+        {reader : "Albert3", give_date:"04/11/2008",return_date:""},
+    ]
+}
+
+const index = book.pickList.findIndex(item => item.reader === "Albert2");
+book.pickList[index] = {...book.pickList[index],return_date:"2/2/2222"}
+console.log(book.pickList)
---
+++ src/service/AccountService.ts
@@ -0,0 +1,10 @@
+import {Reader} from "../model/Reader.js";
+import {Role} from "../utils/libTypes.js";
+
+export interface AccountService {
+    addAccount: (reader:Reader) => Promise<void>;
+    getAccount: (userName:string) => Promise<Reader>;
+    updateAccount:(reader:Reader) => Promise<void>;
+    removeAccount:(userName:string) => Promise<Reader>;
+    updateRoles: (userName: string, roles: Role[]) => Promise<Reader>;
+}
